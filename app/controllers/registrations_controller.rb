a1756e31f315ff0e2c20e8dceb3dd249a900a1bd
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-06-04 10:58:45 +0100

    Update methods in registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,10 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
   
   def sign_up_params
-  params.require(:user).permit(:username, :fullname, :email, :password, :password_confirmation)
+  params.require(:user).permit(:username, :fullname, :email, :password, :password_confirmation, :photo, :cover_image)
   end
   
   def account_update_params
-  params.require(:user).permit(:username, :fullname, :email, :password, :password_confirmation, :current_password)
+  params.require(:user).permit(:username, :fullname, :email, :password, :password_confirmation, :current_password,:photo, :cover_image)
   end
   end 
