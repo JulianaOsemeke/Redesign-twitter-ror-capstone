46934566deeea3909ab934800f159f575a2a59b4
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-06-05 21:40:37 +0100

    Update routes.rb file: followings route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :opinions
   devise_for :users, controllers: { registrations: 'registrations' } 
+  resources :followings, only: [:create, :destroy]
   root 'opinions#index'
+  
 end
