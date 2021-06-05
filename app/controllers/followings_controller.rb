6c71a5dd58f828bf9c00a5ef7a7fc8e77e83e8f3
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-06-05 21:48:11 +0100

    Create followings controller: Add methods

---
+++ app/controllers/followings_controller.rb
@@ -0,0 +1,20 @@
+class FollowingsController < ApplicationController
+before_action :authenticate_user!
+before_action :find_user
+
+def create
+  current_user.follow(@user)
+  redirect_to @user
+end
+
+def destroy
+  current_user.unfollow(@user)
+  redirect_to @user
+end
+
+
+private
+def find_user
+  @user = User.find(params [:user_id])
+end
+
