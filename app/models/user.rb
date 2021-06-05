a4e07203546b2f73911fab38cff0a3bd76c0c142
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-06-05 21:44:27 +0100

    Update associations and validations in user.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :opinions
+         validates :username, presence: true, length: { minimum: 4 , maximum: 20 }
+         validates :fullname, presence: true, length: { minimum: 6 , maximum: 80 }
+
          has_one_attached :photo
          has_one_attached :cover_image
+
+         has_many :opinions
+         has_many :active_followings, class_name: "Following", foreign_key: "follower_id", dependent: :destroy
+         has_many :passive_followings, class_name: "Following", foreign_key: "followed_id", dependent: :destroy
+
+         has_many :following, through: :active_followings, source: :followed
+         has_many :followers, through: :passive_followings, source: :follower
+
+         def follow(user)
+            active_followings.create(followed_id: user.id)
+         end
+
+         def unfollow(user)
+            active_followings.find_by(followed_id: user.id).destroy
+         end
+
+         def following?(user)
+           following.include?(user)
+         end
 end
