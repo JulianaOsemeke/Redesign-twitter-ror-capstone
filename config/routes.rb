e4cd70906c9bd131441ef3d22a974aa1c2232ccd
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-06-07 14:39:12 +0100

    Edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/home'
   resources :opinions
   devise_for :users, controllers: { registrations: 'registrations' } 
   resources :followings, only: [:create, :destroy]
-  root 'opinions#index'
+  root 'users#home'
   
 end
