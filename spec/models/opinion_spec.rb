36956ef27b0dc1cabc4d7637aa809fb751456e5e
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-06-13 14:20:32 +0100

    Add tests

--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -1,3 +1,10 @@
 RSpec.describe Opinion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  current_user = User.first_or_create!(email:'dean@gmail.com', password: 'password', password_confirmation: 'password')
+  it 'has an opinion at least 280 characters long' do
+    opinion = Opinion.new{
+      opinion = 'This is a valid opinion'
+    }
+    expect(opinion).to_be_valid
+  end
+  
 end
